d88e9a1f22df0fdd7c09488439d5898b8582ac66
Motzkiste <motzkiste@proton.me>
2023-04-26 23:25:42 +0200

    add runFolia Task

--- gradle/module-resources/folia.kts
+++ gradle/module-resources/folia.kts
@@ -1,11 +1,13 @@
 plugins {
-//  alias(libs.plugins.run.paper)
+    alias(libs.plugins.run.paper)
 }
 
 dependencies {
     compileOnly(libs.minecraft.server.folia)
 }
 
-//tasks.runServer {
-//  minecraftVersion("1.19.4")
-//}
+tasks {
+    runPaper.folia.registerTask {
+        minecraftVersion("1.19.4")
+    }
+}
